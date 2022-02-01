470d47a21222a22e0de111f1b41c3ede4800a5b4
Humajiva <humajiva123@gmail.com>
2022-02-01 02:09:51 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+//this component will only takes numbers as props and display it in different box
+export const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div
+			className="counter text-center d-flex justify-content-center mt-5 bg-dark"
+			display="flex">
+			<div className="timersymbol text-white p-3 bg-info m-1 rounded">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="text-white p-3 bg-info m-1 rounded">
+				{props.number1}
+			</div>
+			<div className="text-white p-3 bg-info m-1 rounded">
+				{props.number2}
+			</div>
+			<div className="text-white p-3 bg-info m-1 rounded">
+				{props.number3}
+			</div>
+			<div className=" text-white p-3 bg-info m-1 rounded">
+				{props.number4}
+			</div>
+			<div className=" text-white p-3 bg-info m-1 rounded">
+				{props.number5}
+			</div>
+			<div className=" text-white p-3 bg-info m-1 rounded">
+				{props.number6}
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+Home.propTypes = {
+	number1: PropTypes.number,
+	number2: PropTypes.number,
+	number3: PropTypes.number,
+	number4: PropTypes.number,
+	number5: PropTypes.number,
+	number6: PropTypes.number,
+};
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,28 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Home } from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seconds = 0;
+setInterval(() => {
+	seconds++;
+	let one = Math.floor(seconds / 1);
+	let two = Math.floor(seconds / 10);
+	let three = Math.floor(seconds / 100);
+	let four = Math.floor(seconds / 1000);
+	let five = Math.floor(seconds / 10000);
+	let six = Math.floor(seconds / 100000);
+	console.log(one);
+	ReactDOM.render(
+		<Home
+			number1={six % 10}
+			number2={five % 10}
+			number3={four % 10}
+			number4={three % 10}
+			number5={two % 10}
+			number6={one % 10}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
